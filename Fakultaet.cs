927ef0e18cfd4bbafd6469b505b3218c39ac3b6a
d <d>
2024-05-10 09:23:48 +0200

    added aufgabe

---
+++ Fakultaet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace idk;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(fak(50000000));
+    }
+    public static int fak(int x)
+    {
+        int a = 1;
+        for (int i = x; i > 0; i--) // i--;
+        {
+            a = a * i;
+        }
+        // for loop
+        return a;
+    }
+
+
+
+
+
+
+
+}
